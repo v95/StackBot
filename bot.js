25248bf65166dfe41d6a1fd762f70fcbce3cf83b
Stuart Blackler <stuart@sblackler.net>
2012-11-28 20:42:27 +0000

    update to try and circumvent chat command#

--- bot.js
+++ bot.js
@@ -1,27 +1,29 @@
 
 var bot = function(){
-
-/*
-
-		Public Methods
-
-*/
-
+	var  = "#sayit-button";
+	var _origialDefinition = $._data($(_buttonName).get(0), "events").click[0].handler;
+	
+	var _enabled = false;
 
 	var _init = function(){
+		$(_buttonName).unbind('click'); 
+		$(_buttonName).click(test);
 		alert('Test bot loaded');
 	};
 
-/*
 
-		Private Methods
-
-*/	
-	
+	var test = function(){
+		if(this._enabled){
+		    alert("hopefully i blocked the command...");
+			return;
+		}
+		_origialDefinition(this);
+	}
 	
 	// return all the methods that we want to expose.
 	return {
-		init : _init
+		init : _init,
+		test : test
 	}
 
 }();
