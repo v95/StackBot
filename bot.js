8562d95cbb87c40bbdaf990fdda92aa9711a3a54
Stuart Blackler <stuart@sblackler.net>
2012-11-28 20:15:57 +0000

    updated to try the basic bot pattern

--- bot.js
+++ bot.js
@@ -1,1 +1,30 @@
-alert('Test bot loaded');
+
+var bot = function(){
+
+/*
+
+		Public Methods
+
+*/
+
+
+	var _init = function(){
+		alert('Test bot loaded');
+	};
+
+/*
+
+		Private Methods
+
+*/	
+	
+	
+	// return all the methods that we want to expose.
+	return {
+		init : init
+	}
+
+}();
+
+// initalise the bot
+bot.init();
