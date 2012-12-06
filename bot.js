e1a692101f4cdc6d2c022a09db7c015a4ae26334
Stuart Blackler <stuart@sblackler.net>
2012-12-06 23:01:16 +0000

    Added a few new commands.

--- bot.js
+++ bot.js
@@ -15,6 +15,9 @@ var bot = function(){
 	
 	// whether the bot is enabled or not
 	var _isEnabled = true;
+	
+	// the command dictionary
+	var _commands = {};
 
 	// inital configuration for event overriding
 	var _init = function(){
@@ -44,10 +47,10 @@ var bot = function(){
 		_status();
 	};
 
-	// 
+	// Fires when the user clicks send
 	var _buttonChatCommand = function(){
 		if(_isBotCommand && _isEnabled){
-		    alert("Command blocked");
+			_processCommand($(_inputName).val());
 			return;
 		}
 		_buttonSendCommand(this);
@@ -83,8 +86,53 @@ var bot = function(){
 		// just echo to the console for now
 		console.log(_logPrefix + " processing command '" + cmd + "'");
 		
+		// initial string split
+		var cmdAndArgs = str.split(" ");
+		
+		// to store the args
+		var args = new Array();
+		
+		// rebuild the args array
+		for(int i = 1; i < cmdAndArgs.length; i++){
+			args[i - 1] = cmdAndArgs[i];
+		}
+		
+		// execute the commands
+		_execCommand(cmdAndArgs[0].substring(_botPrefix.length).toLowerCase(), args);
+		
 		// clear the command once we are done
 		$(_inputName).val("");
+	}
+	
+	var _execCommand = function(cmd, args){
+		
+		// temp debug
+		console.log(_logPrefix + " executing command -> " + cmd);
+		for(int i = 0; i < args.length; i++){
+			console.log("    arg for command -> " + args[i]);
+		}
+		// end of temp debug		
+
+		// check to see if the command is in the dictionary
+		if(_commands[cmd])
+		{
+			// execute the command
+			_commands[cmd](args);
+		}
+		
+		return;
+	}
+	
+	// to register a command
+	var _registerCommand = function(cmds, args, func){
+		
+		return;
+	}
+	
+	// to unregister a command
+	var _unregisterCommand = function(cmd){
+	
+		return;
 	}
 	
 	// print the status to the console
@@ -99,13 +147,16 @@ var bot = function(){
 	
 	// return all the methods that we want to expose.
 	return {
+		exec : _execCommand,
 		init : _init,
 		isCommand: _isBotCommand,
 		on : _botOn,
 		off : _botOff,
 		processCommand : _processCommand,
+		register : _registerCommand,
 		status : _status,
-		test : _keypressSendCommand
+		test : _keypressSendCommand,
+		unregister : _unregisterCommand
 	}
 
 }();
